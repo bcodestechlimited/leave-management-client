c0b37768f83da127eb397e5ec7a1210494a74ac0
ObodoakorDavid <obodoakordavid@gmail.com>
2025-05-12 15:24:00 +0100

    final

--- src/pages/tenant/tenant-leave.tsx
+++ src/pages/tenant/tenant-leave.tsx
@@ -42,7 +42,10 @@ export default function TenantLeave() {
       const url = window.URL.createObjectURL(new Blob([blob]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "monthly-leave-report.xlsx"); // Adjust name/extension as needed
+      link.setAttribute(
+        "download",
+        `leave-report-${format(startDate as Date, "MMMM")}.pdf`
+      ); // Adjust name/extension as needed
       document.body.appendChild(link);
       link.click();
       link.remove();

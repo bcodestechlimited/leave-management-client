045ce924aade70c847dd22b6951b78b6d351a0b8
ObodoakorDavid <obodoakordavid@outlook.com>
2025-07-16 12:44:10 +0100

    changes again

--- src/pages/employee/employee-admin/all-leaves.tsx
+++ src/pages/employee/employee-admin/all-leaves.tsx
@@ -1,5 +1,4 @@
 import { getAllLeaves } from "@/api/leave.api";
-import LeaveRequestsChart from "@/components/charts/leave-request-chart";
 import DataTable from "@/components/table";
 import { formatDate, getStatusClasses } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
@@ -70,7 +69,6 @@ export default function AllLeaves() {
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-lg font-semibold">Leave History</h1>
       </div>
-      <LeaveRequestsChart />
       <DataTable
         columns={columns}
         data={data?.leaveRequests || []}
